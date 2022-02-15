a87c6f65b552f07ca07859fc0106e160e90d2fcc
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2022-02-15 16:31:13 +0100

    test variables line sizes

--- features/support/parse_support.ts
+++ features/support/parse_support.ts
@@ -5,19 +5,34 @@ import { DigitParser } from '../../src/DigitParser';
 
 let given_string: string;
 let given_string_parsed: string;
-const parser = new Parser();
-const digitParser = new DigitParser('?');
+
+let parser = new Parser();
+let digitParser = new DigitParser('?');
 
 Given(/the following line$/, (line: string) => {
+    parser = new Parser();
     given_string = line;
 });
 
+Given(
+    /the following line of digits with (\d+) height and (\d+) width per digits$/,
+    (height: number, width: number, line: string) => {
+        parser = new Parser(width, height);
+        given_string = line;
+    }
+);
+
 Given(/the following digit$/, (digit: string) => {
+    digitParser = new DigitParser('?');
     given_string = digit;
 });
 
 When('I parse this line', () => {
     given_string_parsed = parser.parseLine(given_string);
+});
+
+When(/I parse this line of (\d+) digits$/, (digits: number) => {
+    given_string_parsed = parser.parseLine(given_string, digits);
 });
 
 When('I parse this digit', () => {
@@ -26,4 +41,8 @@ When('I parse this digit', () => {
 
 Then(/I should get (.+)/, (expected: string) => {
     expect(given_string_parsed).to.equal(expected);
+});
+
+Then(/I should not get anything/, () => {
+    expect(given_string_parsed).to.equal('');
 });
