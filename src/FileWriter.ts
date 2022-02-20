0aee6e98a1250559c9fce0e2f3b9c7b3557b0b15
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2022-02-20 13:17:23 +0100

    create class file writer

---
+++ src/FileWriter.ts
@@ -0,0 +1,9 @@
+import { Writer } from './Writer';
+
+export class FileWriter implements Writer {
+    constructor(source: string) {}
+
+    write(input: string): void {}
+
+    writeLine(input: string): void {}
+}
