bf93554c77563e03b41385063ae1abc24e7332ae
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2022-02-20 14:05:55 +0100

    create writer support

--- features/support/writer_support.ts
+++ features/support/writer_support.ts
@@ -1,10 +1,11 @@
 import { DataTable, Given, Then, When } from '@cucumber/cucumber';
 import { FileReader } from '../../src/FileReader';
-import { expect } from 'chai';
+import { assert, expect } from 'chai';
 import { FileError } from '../../src/FileError';
 import { Parser } from '../../src/Parser';
 import { FileWriter } from '../../src/FileWriter';
 import { Writer } from '../../src/Writer';
+import * as fs from 'fs';
 
 let writer: Writer;
 let filename: string = '';
@@ -14,11 +15,32 @@ let thrownErrorMessage: string = '';
 Given(
     /^I want to write some input in the file (.*)$/,
     (givenFilename: string) => {
+        filename = `features/test_files/out/${givenFilename}`;
         thrownError = false;
         thrownErrorMessage = '';
-        filename = `features/test_files/${givenFilename}`;
+
+        if (fs.existsSync(filename)) {
+            fs.unlinkSync(filename);
+        }
+
+        writer = new FileWriter(filename);
     }
 );
 
-When(/^I write$/, (input: string) => {});
-Then(/^my file appended content should be$/, () => {});
+When(/^I write$/, (input: string) => {
+    writer.write(input);
+});
+
+When(/^I write line$/, (input: string) => {
+    writer.writeLine(input);
+});
+
+Then(/^my file content should be$/, (expectedContent: string) => {
+    let fileContent: string;
+    try {
+        fileContent = fs.readFileSync(filename).toString();
+    } catch (e: any) {
+        assert.fail(e.message);
+    }
+    expect(fileContent.toString()).to.eq(expectedContent);
+});
