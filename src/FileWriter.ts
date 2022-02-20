5fdd9afc53c3ffd89dbba924c7fbd0af17149da0
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2022-02-20 14:06:11 +0100

    fill file writer class

--- src/FileWriter.ts
+++ src/FileWriter.ts
@@ -1,9 +1,28 @@
 import { Writer } from './Writer';
+import * as fs from 'fs';
+import { FileError } from './FileError';
 
 export class FileWriter implements Writer {
-    constructor(source: string) {}
+    private readonly source: string;
 
-    write(input: string): void {}
+    constructor(source: string) {
+        this.source = source;
+    }
 
-    writeLine(input: string): void {}
+    write(input: string): void {
+        try {
+            fs.writeFileSync(this.source, input, {
+                encoding: 'utf-8',
+                flag: 'a+',
+            });
+        } catch (e: any) {
+            throw new FileError(
+                `Error while writing into file '${this.source}'\n${e.message}`
+            );
+        }
+    }
+
+    writeLine(input: string): void {
+        this.write(input + '\n');
+    }
 }
